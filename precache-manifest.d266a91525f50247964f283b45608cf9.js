c12c92d95e99bf43b1964421c1c684ec42ebcf32
Khalem <33363455+Khalem@users.noreply.github.com>
2020-08-12 02:59:30 +0100

    Updates

---
+++ precache-manifest.d266a91525f50247964f283b45608cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cc74d6e51b115e55d7d4b52214479c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f33d9236c07e7a65acd",
+    "url": "/monsters-rolodex/static/css/main.80af5713.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f33d9236c07e7a65acd",
+    "url": "/monsters-rolodex/static/js/main.7faf77cb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
